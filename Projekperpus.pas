0e1553c3d7b91776cfd5919c4764162193d18480
Brilian06 <brilianjbm@gmail.com>
2024-12-15 22:43:02 +0700

    Menambahkan konstanta, record, dan variabel

--- Projekperpus.pas
+++ Projekperpus.pas
@@ -1,6 +1,21 @@
 program manajemen_buku_perpustakaan
 uses crt;
+const
+    MAX_BUKU = 100;
 
+type
+    Buku = record
+        judul: string[100];
+        penulis: string[50];
+        tahun: integer;
+        kode: string[20];
+        jumlah: integer;
+    end;
+
+var
+    daftarBuku: array[1..MAX_BUKU] of Buku;
+    jumlahBuku: integer;
+    pilihan: integer;
 
 begin
 end.
